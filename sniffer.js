5c5a0216b0ea7b217a2a256d625548cdaecee87b
Simon M <simon.marache@gmail.com>
2015-03-15 22:30:58 +0000

    Comments and Readme added

--- sniffer.js
+++ sniffer.js
@@ -10,15 +10,18 @@ navigator.getUserMedia = (navigator.getUserMedia ||
 
 // Analyser object that can be queried to retrieve microphone data.
 var g_analyser = null;
-var features = { };
 
+// Global map contaning all the training data: g_features[key code] = [ <features from sample 1>, <features from sample 2>, ...]
+var g_features = { };
+
+// Return the <li> Node for the given code
 function getCharItem(code) 
 {
     return $$("#sniffList li.sniff" + code + " span.value")
 }
 
-function addChar(code, data) {
-        var values = getCurrentFFTData();
+// a new key has been pressed on the training input field 
+function trainingKeyPressed(code, data) {
         if(!getCharItem(code)) {
             var node = EE('li', { '@class': 'sniff' + code}, [
                             EE('span', { '@class': 'char'  }, String.fromCharCode(code)),
@@ -26,21 +29,23 @@ function addChar(code, data) {
                         ]);
                             
             $("#sniffList").add(node);
-            features[code] = [ ];
+            g_features[code] = [ ];
         }
-        features[code].push(values);
+        g_features[code].push(data);
         var item = getCharItem(code);
         item.textContent = parseInt(item.textContent, 10) + 1;
 }
 
 
-// Return the current fft data captured from the microphone
+// Return the current fft data captured from the microphone.
 function getCurrentFFTData()
 {
     if(g_analyser) {
         var bufferLength = g_analyser.frequencyBinCount;
         var dataArray = new Float32Array(bufferLength);
         // g_analyser.getByteTimeDomainData() // to get more features ?
+        
+        // Question: is the data captured after the call or from just before the call ?
         g_analyser.getFloatFrequencyData(dataArray);
         return dataArray;
     } else {
@@ -64,7 +69,7 @@ $(function() {
     
 
     $('#keyTrain').on('keypress', function(evt) { 
-        addChar(evt.charCode, null);
+        trainingKeyPressed(evt.charCode, getCurrentFFTData());
     });
 
 });
